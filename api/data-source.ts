2f950bab0d097f5145d9fbe1ae949851c86d8215
EnzoColantonio34 <enzo.colantonio@sfr.fr>
2026-03-24 11:31:57 +0100

    feat: Dockerise Back and Front

--- api/data-source.ts
+++ api/data-source.ts
@@ -1,12 +1,17 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { ConfigService } from '@nestjs/config';
 
 export const AppDataSource = new DataSource({
-    type: 'sqlite',
-    database: 'database.sqlite',
+    type: 'postgres',
+    host: ConfigService.get<string>('DB_HOST', 'localhost'),
+    port: ConfigService.get<number>('DB_PORT'),
+    username: ConfigService.get<string>('DB_USER'),
+    password: ConfigService.get<string>('DB_PASSWORD'),
+    database: ConfigService.get<string>('DB_NAME'),
     synchronize: false,
-    entities: ['src/modules/v1/**/*.entity.ts'], 
+    entities:[__dirname + '/**/*.entity{.ts,.js}'], 
     migrations: ['src/migrations/*{.ts,.js}'],
     namingStrategy: new SnakeNamingStrategy(),
 });
